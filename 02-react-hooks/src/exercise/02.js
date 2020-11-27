ecc1f99f7f363122da8565ba104c3e996cd76992
infantito <danielinfantito61@gmail.com>
2020-11-26 21:17:23 -0500

    feat: ✨ custom hook

--- 02-react-hooks/src/exercise/02.js
+++ 02-react-hooks/src/exercise/02.js
@@ -3,18 +3,24 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  const [name, setName] = React.useState(
-    () => window.localStorage.getItem('name') || initialName,
+function useLocalStorage(key, defaultValue = '') {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) || defaultValue,
   )
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  }, [key, state])
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorage('name', initialName)
 
   function handleChange(event) {
     setName(event.target.value)
   }
-
-  React.useEffect(() => {
-    window.localStorage.setItem('name', name)
-  }, [name])
 
   return (
     <div>
