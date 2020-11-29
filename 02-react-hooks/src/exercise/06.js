e0b87180dafeab46c39fb552f2daf6fe49ada9a8
infantito <danielinfantito61@gmail.com>
2020-11-29 15:59:11 -0500

    fix: 🐛 use resetKeys

--- 02-react-hooks/src/exercise/06.js
+++ 02-react-hooks/src/exercise/06.js
@@ -79,7 +79,11 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <ErrorBoundary FallbackComponent={ErrorFallback} onReset={handleReset}>
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback}
+          onReset={handleReset}
+          resetKeys={[pokemonName]}
+        >
           <PokemonInfo pokemonName={pokemonName} />
         </ErrorBoundary>
       </div>
