6721e3c37c7c624aef68c525365b0b73a4442824
infantito <danielinfantito61@gmail.com>
2020-12-09 21:53:19 -0500

    feat: ✨ context module functions

--- 04-advanced-react-patterns/src/exercise/01.js
+++ 04-advanced-react-patterns/src/exercise/01.js
@@ -10,6 +10,7 @@ import * as userClient from '../user-client'
 import {useAuth} from '../auth-context'
 
 const UserContext = React.createContext()
+
 UserContext.displayName = 'UserContext'
 
 function userReducer(state, action) {
@@ -55,36 +56,56 @@ function userReducer(state, action) {
 
 function UserProvider({children}) {
   const {user} = useAuth()
+
   const [state, dispatch] = React.useReducer(userReducer, {
     status: null,
     error: null,
     storedUser: user,
     user,
   })
+
   const value = [state, dispatch]
+
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
 
 function useUser() {
   const context = React.useContext(UserContext)
+
   if (context === undefined) {
     throw new Error(`useUser must be used within a UserProvider`)
   }
+
   return context
 }
 
-// 🐨 add a function here called `updateUser`
-// Then go down to the `handleSubmit` from `UserSettings` and put that logic in
-// this function. It should accept: dispatch, user, and updates
+// got this idea from Dan and I love it:
+// https://twitter.com/dan_abramov/status/1125773153584676864
+async function updateUser(dispatch, user, updates) {
+  dispatch({type: 'start update', updates: updates})
 
-// export {UserProvider, useUser}
+  try {
+    const updatedUser = await userClient.updateUser(user, updates)
+
+    dispatch({type: 'finish update', updatedUser})
+
+    return updatedUser
+  } catch (error) {
+    dispatch({type: 'fail update', error})
+
+    return Promise.reject(error)
+  }
+}
+
+// export {UserProvider, useUser, updateUser}
 
 // src/screens/user-profile.js
-// import {UserProvider, useUser} from './context/user-context'
+// import {UserProvider, useUser, updateUser} from './context/user-context'
 function UserSettings() {
   const [{user, status, error}, userDispatch] = useUser()
 
   const isPending = status === 'pending'
+
   const isRejected = status === 'rejected'
 
   const [formState, setFormState] = React.useState(user)
@@ -97,12 +118,10 @@ function UserSettings() {
 
   function handleSubmit(event) {
     event.preventDefault()
-    // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+
+    updateUser(userDispatch, user, formState).catch(() => {
+      /* ignore the error */
+    })
   }
 
   return (
@@ -175,6 +194,7 @@ function UserSettings() {
 
 function UserDataDisplay() {
   const [{user}] = useUser()
+
   return <pre>{JSON.stringify(user, null, 2)}</pre>
 }
 
