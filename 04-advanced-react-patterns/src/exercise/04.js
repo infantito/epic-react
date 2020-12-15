7e9ac4ccc0f5a957681cfc2ff50dc63b789770cb
infantito <danielinfantito61@gmail.com>
2020-12-14 23:10:48 -0500

    perf: ✨ prop getters

--- 04-advanced-react-patterns/src/exercise/04.js
+++ 04-advanced-react-patterns/src/exercise/04.js
@@ -4,29 +4,41 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args))
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
 
   const toggle = () => setOn(!on)
 
+  function getTogglerProps({onClick, ...props} = {}) {
+    return {
+      'aria-pressed': on,
+      onClick: callAll(onClick, toggle),
+      ...props,
+    }
+  }
+
   return {
     on,
     toggle,
-    togglerProps: {
-      'aria-pressed': on,
-      onClick: toggle,
-    },
+    getTogglerProps,
   }
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
 
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
